5974154ef674690b365a7eb94f32596a4010170b
haris <sdevfullstack003@gmail.com>
2024-11-18 12:43:58 +0500

    set password logic update

---
+++ resources/views/emails/otp/PasswordResetOtp.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Reset OTP</title>
+</head>
+<body>
+    <p>Dear User,</p>
+    <p>Your password reset OTP is: <strong>{{ $otp }}</strong></p>
+    <p>This OTP is valid for 30 minutes.</p>
+    <p>If you did not request this, please ignore this email.</p>
+    <p>Thank you,</p>
+    <p>TSP</p>
+</body>
+</html>
