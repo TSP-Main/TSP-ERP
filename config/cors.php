6f1cb5914e15ae0a1f7eac3c45419a1e1524938a
laraibsehrish <laraibsehrish2020@gmail.com>
2024-11-19 10:32:29 +0500

    build 11/19 10:32

--- config/cors.php
+++ config/cors.php
@@ -34,7 +34,7 @@ return [
 
     'paths' => ['api/*', 'oauth/*', 'login', 'register', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://127.0.0.1:8000'], // Replace with your frontend URL
+    'allowed_origins' => ['https://rota.tspsolution.com'], // Replace with your frontend URL
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
