132d43061412cfb7299fed373b3fa7b086e3abfe
haris <sdevfullstack003@gmail.com>
2024-11-28 14:04:36 +0500

    statuses apis coverd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Event::listen(\Illuminate\Auth\Events\Authenticated::class, function ($event) {
-            logger('Authenticated event fired for user ID: ' . $event->user->id);
-        });
+        //
     }
 }
