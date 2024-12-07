f5b8ed4efdff3736d459495ef11430923722a270
laraibsehrish <laraibsehrish2020@gmail.com>
2024-12-07 14:10:17 +0500

    12/7 2 10

--- resources/js/Profile/Profile.jsx
+++ resources/js/Profile/Profile.jsx
@@ -1,74 +1,85 @@
-import React, { useEffect } from 'react'
-import book from './company.png'
-import money from './money.png'
-import totalemployees from './totalemployess.png'
-import present from './present.png'
-import absent from './absent.png'
+import React, { useEffect } from "react";
+import book from "./company.png";
+import money from "./money.png";
+import totalemployees from "./totalemployess.png";
+import present from "./present.png";
+import absent from "./absent.png";
+import "./profile.css";
 // import { FaCoffee } from "react-icons/fa";
-import StatCard from './StarCard';
+import StatCard from "./StarCard";
 // useEffect(()=>{
 //   const role=localStorage.getItem('role')
 // })
 const Profile = () => {
-   const name=localStorage.getItem('name')
-   const role=localStorage.getItem('role')
-  
-  return (
-      <div
-          style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-          }}
-      >
-          <div>
-              <h2>Welcome Back, {name}!</h2>
-              <p>Here's Your study</p>
-          </div>
-          {role == "super_admin" && (
-              <>
-                  <StatCard icon={book} total={10} text="Total Companies" />
-                  <StatCard icon={money} total={10} text="Total Revenue" />
-              </>
-          )}
-          {role == "company" && (
-              <>
-                  <StatCard
-                      icon={totalemployees}
-                      total={10}
-                      text="Total Employees"
-                  />
-                  <StatCard icon={present} total={10} text="Checked In" />
-                  <StatCard icon={absent} total={10} text="Absent" />
-              </>
-          )}
-          {role == "manager" && (
-              <>
-                  <StatCard
-                      icon={totalemployees}
-                      total={10}
-                      text="Total Employees"
-                  />
-                  <StatCard icon={present} total={10} text="Checked In" />
-                  <StatCard icon={absent} total={10} text="Absent" />
-              </>
-          )}
-          {role == "employee" && (
-              <>
-                  <StatCard
-                      icon={totalemployees}
-                      total={10}
-                      text="Total Shifts"
-                  />
-                  <StatCard icon={present} total={10} text="Attended" />
-                  <StatCard icon={absent} total={10} text="Missed" />
-              </>
-          )}
+    const name = localStorage.getItem("name");
+    const role = localStorage.getItem("role");
 
-          <div></div>
-      </div>
-  );
-}
+    return (
+        <div
+            className="profile-dash"
+            //   style={{
+            //       display: "flex",
+            //       flexDirection: "row",
+            //       justifyContent: "space-between",
+            //       alignItems: "center",
+            //   }}
+        >
+            <div
+                style={{
+                    lineHeight: "1px",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "space-around",
+                    // paddingRight: "30px",
+                }}
+            >
+                <h2>Welcome Back </h2>
+                <h3>{name}!</h3>
+            </div>
+            {role == "super_admin" && (
+                <div className="starr-card">
+                    <StatCard icon={book} total={10} text="Total Companies" />
+                    <StatCard icon={money} total={10} text="Total Revenue" />
+                </div>
+            )}
+            {role == "company" && (
+                <div className="starr-card">
+                    <StatCard
+                        icon={totalemployees}
+                        total={10}
+                        text="Total Employees"
+                    />
+                    <StatCard icon={present} total={10} text="Checked In" />
+                    <StatCard icon={absent} total={10} text="Absent" />
+                </div>
+            )}
+            {role == "manager" && (
+                <div className="starr-card">
+                    <StatCard
+                        icon={totalemployees}
+                        total={10}
+                        text="Total Employees"
+                    />
+                    <StatCard icon={present} total={10} text="Checked In" />
+                    <StatCard icon={absent} total={10} text="Absent" />
+                </div>
+            )}
+            {role == "employee" && (
+                <div className="starr-card">
+                    <StatCard
+                        icon={totalemployees}
+                        total={10}
+                        text="Total Shifts"
+                    />
+                    <StatCard icon={present} total={10} text="Attended" />
+                    <StatCard icon={absent} total={10} text="Missed" />
+                </div>
+            )}
 
-export default Profile
+            <div></div>
+        </div>
+    );
+};
+
+export default Profile;
